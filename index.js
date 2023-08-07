4238ad395c54cafc8e77b10cc4faf6ec58406a8e
keatonshall <83672762+keatonshall@users.noreply.github.com>
2023-08-07 15:31:00 -0500

    Clock in/out distinction and *-covered PINs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let x = localStorage.getItem("unlocked");
 let unlocked=true;
+let pinTyped = "";
+
 if(x==undefined){
 unlocked = false;
 }
@@ -13,7 +15,7 @@ addEventListener("keydown", (event) => {
 
     console.log(event.key);
 
-    if(event.key == "Enter" && document.getElementById("pin").innerHTML==271828){
+    if(event.key == "Enter" && pinTyped==271828){
     //  submit();
     unlocked = true;
     localStorage.setItem("unlocked", true);
@@ -21,10 +23,12 @@ addEventListener("keydown", (event) => {
       document.getElementById("name").innerHTML = "";
       document.getElementById("logo").style.width = '500px';
         document.getElementById("logo").style.height = '375px';
+        pinTyped = "";
 
     }else if(event.key == "Backspace"){
       document.getElementById("pin").innerHTML = document.getElementById("pin").innerHTML.slice(0, -1);
-      let name = findName(document.getElementById("pin").innerHTML);
+      pinTyped = pinTyped.slice(0, -1);
+
       document.getElementById("name").innerHTML = "Admin";
       if(document.getElementById("pin").innerHTML == ""){
           document.getElementById("name").innerHTML = "Admin Password";
@@ -35,8 +39,9 @@ addEventListener("keydown", (event) => {
       let key = parseInt(event.key);
       if(key>=0){
         if(document.getElementById("pin").innerHTML.length<6){
-      document.getElementById("pin").innerHTML += key;
-      let name = findName(document.getElementById("pin").innerHTML);
+      document.getElementById("pin").innerHTML += "*";
+      pinTyped+= key
+
       document.getElementById("name").innerHTML = "Admin";
       document.getElementById("logo").style.width = '0px';
         document.getElementById("logo").style.height = '0px';
@@ -51,28 +56,43 @@ addEventListener("keydown", (event) => {
   }else{
   console.log(event.key);
 
-  if(event.key == "Enter" && findName(document.getElementById("pin").innerHTML) != "Not Valid"){
-    submit();
+  if(event.key == "Enter" && findName(pinTyped) != "Not Valid"){
+    submit(document.getElementById("clockDirection").innerHTML);
     document.getElementById("pin").innerHTML = "";
+    pinTyped="";
     document.getElementById("name").innerHTML = "";
     document.getElementById("logo").style.width = '500px';
       document.getElementById("logo").style.height = '375px';
+      document.getElementById("clockDirection").innerHTML="";
+
 
   }else if(event.key == "Backspace"){
-    document.getElementById("pin").innerHTML = document.getElementById("pin").innerHTML.slice(0, -1);
-    let name = findName(document.getElementById("pin").innerHTML);
-    document.getElementById("name").innerHTML = (name=="Not Valid")?"":name;
     if(document.getElementById("pin").innerHTML == ""){
         document.getElementById("name").innerHTML = "";
       document.getElementById("logo").style.width = '500px';
         document.getElementById("logo").style.height = '375px';
+        document.getElementById("clockDirection").innerHTML="";
     }
+    document.getElementById("pin").innerHTML = document.getElementById("pin").innerHTML.slice(0, -1);
+    pinTyped=pinTyped.slice(0, -1);
+    let name = findName(pinTyped.innerHTML);
+    document.getElementById("name").innerHTML = (name=="Not Valid")?"":name;
+
+  }else if(event.key=="+" || event.key=="-"){
+    console.log("signalling clock direction")
+    document.getElementById("clockDirection").innerHTML = (event.key=="-")? "In":"Out"
+    document.getElementById("logo").style.width = '0px';
+      document.getElementById("logo").style.height = '0px';
+
   }else{
+
+
     let key = parseInt(event.key);
-    if(key>=0){
+    if(key>=0 && document.getElementById("clockDirection").innerHTML.length !="0"){
       if(document.getElementById("pin").innerHTML.length<4){
-    document.getElementById("pin").innerHTML += key;
-    let name = findName(document.getElementById("pin").innerHTML);
+    document.getElementById("pin").innerHTML += "*";
+    pinTyped+= key;
+    let name = findName(pinTyped);
     document.getElementById("name").innerHTML = (name=="Not Valid")?"":name;
     document.getElementById("logo").style.width = '0px';
       document.getElementById("logo").style.height = '0px';
@@ -82,9 +102,10 @@ addEventListener("keydown", (event) => {
 }
 })
 
-function submit(){
-  let pin = document.getElementById("pin").innerHTML;
-  fetch("https://docs.google.com/forms/d/e/1FAIpQLSdWdMnapSuoJuENKVPyx0qksRUrDHAVPaKbHlOD7HtnKSZ2Zg/formResponse?entry.101403101="+pin);
+function submit(clockingDirection){
+  let pin = pinTyped;
+
+  fetch("https://docs.google.com/forms/d/e/1FAIpQLSdWdMnapSuoJuENKVPyx0qksRUrDHAVPaKbHlOD7HtnKSZ2Zg/formResponse?usp=pp_url&entry.101403101="+pin+"&entry.1517704973="+clockingDirection+"&submit=Submit");
 }
 
 function findName(pin){
